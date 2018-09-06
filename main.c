2b7233c01e117e9693851f9dff875e30c5dbc86b
kneo002 <kneo002@e.ntu.edu.sg>
2018-09-06 16:42:46 +0800

    added clock setup to 80MHz

--- main.c
+++ main.c
@@ -28,23 +28,23 @@ static void
 task1(void *args) {
 
 		int c = '0' - 1;
-
+    
 		for (;;) {
-			vTaskDelay(pdMS_TO_TICKS(500)); //500ms Delay
+			gpio_toggle(GPIOB,GPIO3);
+			vTaskDelay(pdMS_TO_TICKS(1000)); //500ms Delay
 			if ( ++c >= 'Z' ) {
-				uart_putc(c);
-				uart_putc('\r');
-				uart_putc('\n');
-				c = '0' - 1;
+			 	uart_putc(c);
+			 	uart_putc('\r');
+			 	uart_putc('\n');
+			 	c = '0' - 1;
 			} else	{
-				uart_putc(c);
+			 	uart_putc(c);
 			}
 		}
 }
 
 static void uart_setup(void) {
 
-	 rcc_periph_clock_enable(RCC_USART1);
 	 usart_set_baudrate(USART1, 9600);
 	 usart_set_mode(USART1, USART_MODE_TX);
 	 usart_set_databits(USART1, 8);
@@ -61,18 +61,48 @@ static void gpio_setup(void)
 	gpio_set_af(GPIOB, GPIO_AF7, GPIO6);
 }
 
+static void clock_setup(void)
+{
+	/* FIXME - this should eventually become a clock struct helper setup */
+	rcc_osc_on(RCC_HSI16);
+
+	flash_prefetch_enable();
+	flash_set_ws(4);
+	flash_dcache_enable();
+	flash_icache_enable();
+	/* 16MHz / 4 = > 4 * 40 = 160MHz VCO => 80MHz main pll  */
+	rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40,
+			0, 0, RCC_PLLCFGR_PLLR_DIV2);
+	rcc_osc_on(RCC_PLL);
+	/* either rcc_wait_for_osc_ready() or do other things */
+
+	/* Enable clocks for the ports we need */
+
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_USART1);
+
+
+	rcc_set_sysclk_source(RCC_CFGR_SW_PLL); /* careful with the param here! */
+	rcc_wait_for_sysclk_status(RCC_PLL);
+	/* FIXME - eventually handled internally */
+	rcc_ahb_frequency = 80e6;
+	rcc_apb1_frequency = 80e6;
+	rcc_apb2_frequency = 80e6;
+}
+
 int
 main(void) {
 
-	rcc_periph_clock_enable(RCC_GPIOB);
 
+  clock_setup();
 	gpio_setup();
 	uart_setup();
-
-	xTaskCreate(task1,"USART",100,NULL,configMAX_PRIORITIES-1,NULL);
+	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP , GPIO3);
+	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
 	vTaskStartScheduler();
 
 	for (;;);
 
 	return 0;
 }
+
